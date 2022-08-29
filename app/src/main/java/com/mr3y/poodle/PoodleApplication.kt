9f012150dacd83f500101640db0d484bc052dc81
MR3Y <abdonasr379@gmail.com>
2022-08-29 15:38:35 +0200

    integrate Napier logger & StrictMode

--- app/src/main/java/com/mr3y/poodle/PoodleApplication.kt
+++ app/src/main/java/com/mr3y/poodle/PoodleApplication.kt
@@ -1,7 +1,35 @@
 package com.mr3y.poodle
 
 import android.app.Application
+import android.os.StrictMode
 import dagger.hilt.android.HiltAndroidApp
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
 
 @HiltAndroidApp
-class PoodleApplication : Application()
+class PoodleApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            setUpNapierLogger()
+            setUpStrictMode()
+        }
+    }
+
+    private fun setUpNapierLogger() {
+        Napier.base(DebugAntilog())
+    }
+
+    private fun setUpStrictMode() {
+        StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder().detectAll().penaltyDeath().build())
+        StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+                .detectActivityLeaks()
+                .detectCleartextNetwork()
+                .penaltyDeath()
+                .build()
+        )
+    }
+}
