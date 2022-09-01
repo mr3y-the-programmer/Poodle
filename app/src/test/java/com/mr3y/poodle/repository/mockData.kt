5516d98dbf4796b3a18b2bed7d6cc0e153cf20ea
MR3Y <abdonasr379@gmail.com>
2022-09-01 15:23:41 +0200

    Fix broken SearchForArtifactsRepositoryImplTest

--- app/src/test/java/com/mr3y/poodle/repository/mockData.kt
+++ app/src/test/java/com/mr3y/poodle/repository/mockData.kt
@@ -1,5 +1,9 @@
 package com.mr3y.poodle.repository
 
+import com.mr3y.poodle.network.fakeMavenCentralDeserializedResponse
+import com.mr3y.poodle.network.filteredFakeJitPackDeSerializedResponse
+import java.time.Instant
+import java.time.ZoneId
 import java.time.ZonedDateTime
 
 internal val fakeSearchQuery = SearchQuery("zhuinden", "", 100, "", emptySet(), "", "")
@@ -9,19 +13,19 @@ internal val fakeArtifacts = listOf(
         "io.github.a914-gowtham:compose-ratingbar",
         "1.2.3",
         "aar",
-        ZonedDateTime.parse("2022-02-04T20:47:08+02:00[Africa/Cairo]")
+        ZonedDateTime.ofInstant(Instant.ofEpochMilli(fakeMavenCentralDeserializedResponse.response.docs[0].timestamp), ZoneId.systemDefault())
     ),
     Artifact.MavenCentralArtifact(
         "de.charlex.compose:speeddial",
         "1.0.0-beta03",
         "aar",
-        ZonedDateTime.parse("2021-06-16T20:34:06+02:00[Africa/Cairo]")
+        ZonedDateTime.ofInstant(Instant.ofEpochMilli(fakeMavenCentralDeserializedResponse.response.docs[1].timestamp), ZoneId.systemDefault())
     ),
-    Artifact.JitPackArtifact("com.github.zhuinden:simple-stack-extensions", "1.0.1", ZonedDateTime.parse("2020-12-14T18:23:59.390+02:00[Africa/Cairo]")),
-    Artifact.JitPackArtifact("com.github.zhuinden:simple-stack", "1.0.1", ZonedDateTime.parse("2020-12-14T18:23:59.390+02:00[Africa/Cairo]")),
-    Artifact.JitPackArtifact("com.github.zhuinden:state-bundle", "1.0.1", ZonedDateTime.parse("2020-12-14T18:23:59.390+02:00[Africa/Cairo]")),
-    Artifact.JitPackArtifact("com.github.zhuinden:realm-monarchy", "1.0.1", ZonedDateTime.parse("2020-12-14T18:23:59.390+02:00[Africa/Cairo]")),
-    Artifact.JitPackArtifact("com.github.zhuinden:fragmentviewbindingdelegate-kt", "1.0.1", ZonedDateTime.parse("2020-12-14T18:23:59.390+02:00[Africa/Cairo]")),
+    Artifact.JitPackArtifact("com.github.zhuinden:simple-stack-extensions", "1.0.1", ZonedDateTime.ofInstant(Instant.ofEpochMilli(filteredFakeJitPackDeSerializedResponse.artifacts[0].timestamp!!), ZoneId.systemDefault())),
+    Artifact.JitPackArtifact("com.github.zhuinden:simple-stack", "1.0.1", ZonedDateTime.ofInstant(Instant.ofEpochMilli(filteredFakeJitPackDeSerializedResponse.artifacts[1].timestamp!!), ZoneId.systemDefault())),
+    Artifact.JitPackArtifact("com.github.zhuinden:state-bundle", "1.0.1", ZonedDateTime.ofInstant(Instant.ofEpochMilli(filteredFakeJitPackDeSerializedResponse.artifacts[2].timestamp!!), ZoneId.systemDefault())),
+    Artifact.JitPackArtifact("com.github.zhuinden:realm-monarchy", "1.0.1", ZonedDateTime.ofInstant(Instant.ofEpochMilli(filteredFakeJitPackDeSerializedResponse.artifacts[3].timestamp!!), ZoneId.systemDefault())),
+    Artifact.JitPackArtifact("com.github.zhuinden:fragmentviewbindingdelegate-kt", "1.0.1", ZonedDateTime.ofInstant(Instant.ofEpochMilli(filteredFakeJitPackDeSerializedResponse.artifacts[4].timestamp!!), ZoneId.systemDefault())),
 )
 
 internal val fakeArtifactsPart1 = fakeArtifacts.take(2)
