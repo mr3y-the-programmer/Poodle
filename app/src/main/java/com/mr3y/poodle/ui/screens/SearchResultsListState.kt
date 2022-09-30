ea6fbfd085e4e57933cf2346c368a36100e09496
M R 3 Y <26522145+mr3y-the-programmer@users.noreply.github.com>
2022-09-30 13:54:12 +0200

    Persist Selected tab's current page state across configuration changes (#6) [ci skip]
    * Extract paginating results to a separate class
    
    * Avoid displaying header when we have exactly one page
    
    * persist current page state across configuration changes

---
+++ app/src/main/java/com/mr3y/poodle/ui/screens/SearchResultsListState.kt
@@ -0,0 +1,66 @@
+package com.mr3y.poodle.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.Saver
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import com.mr3y.poodle.repository.Artifact
+
+const val DefaultPageSize = 20
+
+@Composable
+fun rememberSearchResultsListState(
+    artifacts: List<Artifact>,
+    totalNumOfAllMatchedArtifacts: Int = artifacts.size,
+    numOfArtifactsPerPage: Int = DefaultPageSize
+): SearchResultsListState {
+    return rememberSaveable(artifacts, saver = SearchResultsListState.Saver()) {
+        SearchResultsListState(totalNumOfAllMatchedArtifacts, numOfArtifactsPerPage)
+    }
+}
+
+class SearchResultsListState(
+    val totalNumOfAllMatchedArtifacts: Int,
+    val numOfArtifactsPerPage: Int
+) {
+    var currentPage by mutableStateOf(1..totalNumOfAllMatchedArtifacts.coerceAtMost(numOfArtifactsPerPage))
+        private set
+
+    val isFirstPage by derivedStateOf { currentPage.first == 1 }
+
+    val isLastPage by derivedStateOf { currentPage.last == totalNumOfAllMatchedArtifacts }
+
+    fun goToNextPage() {
+        currentPage = if ((totalNumOfAllMatchedArtifacts - currentPage.last) < numOfArtifactsPerPage)
+            (currentPage.last + 1)..(totalNumOfAllMatchedArtifacts)
+        else
+            (currentPage.first + numOfArtifactsPerPage)..(currentPage.last + numOfArtifactsPerPage)
+    }
+
+    fun backToThePreviousPage() {
+        currentPage = if (totalNumOfAllMatchedArtifacts == currentPage.last) {
+            val subtracted = if (totalNumOfAllMatchedArtifacts % numOfArtifactsPerPage == 0) numOfArtifactsPerPage else (totalNumOfAllMatchedArtifacts % numOfArtifactsPerPage)
+            (currentPage.first - numOfArtifactsPerPage)..(currentPage.last - subtracted)
+        } else
+            (currentPage.first - numOfArtifactsPerPage)..(currentPage.last - numOfArtifactsPerPage)
+    }
+
+    fun getCurrentPageArtifactsOf(allArtifacts: List<Artifact>) = allArtifacts.slice((currentPage.first - 1) until currentPage.last)
+
+    companion object {
+
+        fun Saver() = Saver<SearchResultsListState, IntArray>(
+            save = { state ->
+                intArrayOf(state.totalNumOfAllMatchedArtifacts, state.numOfArtifactsPerPage, state.currentPage.first, state.currentPage.last)
+            },
+            restore = {
+                val restored = SearchResultsListState(it[0], it[1])
+                restored.currentPage = it[2]..it[3]
+                restored
+            }
+        )
+    }
+}
---
+++ app/src/test/java/com/mr3y/poodle/ui/screens/SearchResultsListStateTest.kt
@@ -0,0 +1,73 @@
+package com.mr3y.poodle.ui.screens
+
+import com.google.common.truth.Truth.assertThat
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import com.google.testing.junit.testparameterinjector.TestParameters
+import com.mr3y.poodle.repository.fakeArtifacts
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(TestParameterInjector::class)
+class SearchResultsListStateTest {
+
+    private lateinit var sut: SearchResultsListState
+
+    @Test
+    @TestParameters("{additionalValue: 0}")
+    @TestParameters("{additionalValue: 10}")
+    fun `given a number of artifacts per page that is higher than or equal to all matched artifacts number, then verify we have exactly one page`(
+        additionalValue: Int
+    ) {
+        sut = SearchResultsListState(fakeArtifacts.size, fakeArtifacts.size + additionalValue)
+
+        assertThat(sut.currentPage).isEqualTo(1..fakeArtifacts.size)
+        assertThat(sut.getCurrentPageArtifactsOf(fakeArtifacts)).isEqualTo(fakeArtifacts)
+        assertThat(sut.isFirstPage).isTrue()
+        assertThat(sut.isLastPage).isTrue()
+    }
+
+    @Test
+    fun `given a number of matched artifacts that is higher than number of artifacts per page, then verify we have more than one page`() {
+        val artifacts = buildList {
+            addAll(fakeArtifacts)
+            addAll(fakeArtifacts)
+            addAll(fakeArtifacts)
+        }
+        val numberOfArtifactsPerPage = artifacts.size - 11
+        sut = SearchResultsListState(artifacts.size, numberOfArtifactsPerPage)
+
+        fun assertWeAreOnFirstPage() {
+            assertThat(sut.currentPage).isEqualTo(1..numberOfArtifactsPerPage)
+            assertThat(sut.getCurrentPageArtifactsOf(artifacts)).isEqualTo(artifacts.subList(0, numberOfArtifactsPerPage))
+            assertThat(sut.isFirstPage).isTrue()
+            assertThat(sut.isLastPage).isFalse()
+        }
+        assertWeAreOnFirstPage()
+
+        sut.goToNextPage()
+
+        fun assertWeAreOnSecondPage() {
+            assertThat(sut.currentPage).isEqualTo((numberOfArtifactsPerPage + 1)..(numberOfArtifactsPerPage * 2))
+            assertThat(sut.getCurrentPageArtifactsOf(artifacts)).isEqualTo(artifacts.subList(numberOfArtifactsPerPage, (numberOfArtifactsPerPage * 2)))
+            assertThat(sut.isFirstPage).isFalse()
+            assertThat(sut.isLastPage).isFalse()
+        }
+        assertWeAreOnSecondPage()
+
+        sut.goToNextPage()
+
+        // last page
+        assertThat(sut.currentPage).isEqualTo((numberOfArtifactsPerPage * 2 + 1)..(numberOfArtifactsPerPage * 2 + 1))
+        assertThat(sut.getCurrentPageArtifactsOf(artifacts)).isEqualTo(listOf(artifacts[artifacts.lastIndex]))
+        assertThat(sut.isFirstPage).isFalse()
+        assertThat(sut.isLastPage).isTrue()
+
+        sut.backToThePreviousPage()
+
+        assertWeAreOnSecondPage()
+
+        sut.backToThePreviousPage()
+
+        assertWeAreOnFirstPage()
+    }
+}
